7fefb8cc5915f93cf4490d698eba1237ebf2c86e
joy-mug <he258she@gmail.com>
2025-12-20 03:10:02 +0800

    remove legacy src prototype and test data

--- src/test_interference.py
+++
@@ -1,9 +0,0 @@
-print("Python Interference Lab is Online!")
-
-def intercept_test():
-    data = "Original Secret Data"
-    # Simple simulation of interfering with data
-    interfered_data = data.replace("Original", "MODIFIED")
-    print(f"Result: {interfered_data}")
-
-intercept_test()
